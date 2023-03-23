de6004c356dc6573a6161f5f6b5bc98300d6c573
salman4ll <sfarisi0101@gmail.com>
2023-03-24 04:25:52 +0700

    final layouting

--- app/src/main/java/com/example/project/filter_mountain.java
+++ app/src/main/java/com/example/project/filter_mountain.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class filter_mountain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registrasi);
+        setContentView(R.layout.activity_filter_mountain);
     }
 }
